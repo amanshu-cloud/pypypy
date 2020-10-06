c6a7894f86f5dca9ef1eb647ecadab2ef974fff7
Amanshu <55711996+amanshu-cloud@users.noreply.github.com>
2020-10-06 09:50:59 +0530

    returns weekday on date as input

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import datetime
 import calendar
 import sys
 import os
-print("enter date: ")
+print("enter date:  ")
 (date,month,year) = sys.stdin.readline().split() 
 x = datetime.datetime(int(year),int(month),int(date))
 print(calendar.day_name[x.weekday()])
